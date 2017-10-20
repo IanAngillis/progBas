c66d09a26ae94818a74702acfb33c484e37bfd9e
Ian <ianangillis@gmail.com>
2017-10-20 20:05:02 +0200

    Hoofdstuk 6 Extra Oefening 2

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/79/80788de2c0b500171710ec3f076393c8
@@ -0,0 +1,10 @@
+package be.pxl.h6.exoef2;
+
+public class H6ExOef2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- H6/src/be/pxl/h6/exoef2/H6ExOef2.java
+++ H6/src/be/pxl/h6/exoef2/H6ExOef2.java
@@ -1,10 +1,26 @@
 package be.pxl.h6.exoef2;
 
+import java.util.Scanner;
+
 public class H6ExOef2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Geef een tekst in: ");
+		String text = keyboard.nextLine();
+		
+		if(text.length() % 3 == 0) {
+			text = text.toUpperCase();
+			System.out.println("Deelbaar door 3");
+		} else {
+			text = text.toLowerCase();
+			System.out.println("Niet deelbaar door 3");
+		}
+		
+		System.out.println(text);
+		
+		keyboard.close();
 	}
 
 }
