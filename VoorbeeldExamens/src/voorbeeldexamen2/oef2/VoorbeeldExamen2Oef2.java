418c686260d5f2c6d7f6732c41c258e38ea7440e
Ian <ianangillis@gmail.com>
2017-11-07 14:50:05 +0100

    Voorbeeldexamen 2 oef 2 afgewerkt

--- VoorbeeldExamens/src/voorbeeldexamen2/oef2/VoorbeeldExamen2Oef2.java
+++ VoorbeeldExamens/src/voorbeeldexamen2/oef2/VoorbeeldExamen2Oef2.java
@@ -13,6 +13,7 @@ public class VoorbeeldExamen2Oef2 {
 		String[] roman = { "XL", "X", "IX", "V", "IV", "I" };
 		int[] waarde = { 40, 10, 9, 5, 4, 1 };
 
+		int som = 0;
 		int randomGetal;
 		char character;
 		int vorigGetal = -1;
@@ -20,25 +21,55 @@ public class VoorbeeldExamen2Oef2 {
 		System.out.println("Geef letter: ");
 		character = keyboard.next().charAt(0);
 
+		int[] reeks = new int[4];
+
 		for (char i = 'a'; i <= character; i++) {
-			
+
 			System.out.println("Reeks " + i);
 			randomGetal = rand.nextInt(50 - 2) + 1;
-			
-			while(randomGetal > vorigGetal) {
-				
+
+			while (randomGetal > vorigGetal) {
+
 				vorigGetal = randomGetal;
 				romeinsCijfer = zetOmNaarRomeinsCijfer(randomGetal, roman, waarde);
 				System.out.println("Het Romeinse cijfer voor " + randomGetal + " is " + romeinsCijfer);
-				
+				som += randomGetal;
 				randomGetal = rand.nextInt(50 - 2) + 1;
 			}
 			
-		vorigGetal = -1;
+			System.out.println();
+			vorigGetal = -1;
+			vulReeksAan(reeks, som);
+			som = 0;
 
 		}
 
+		printReeksInformatie(reeks);
+
 		keyboard.close();
+	}
+
+	public static void printReeksInformatie(int[] reeks) {
+		
+		System.out.println();
+		System.out.println("Aantal reeksen met een som van gegenereerde getallen minder dan 50: " + reeks[0]);
+		System.out.println("Aantal reeksen met een som van gegenereerde getallen tussen 50 en 70: " + reeks[1]);
+		System.out.println("Aantal reeksen met een som van gegenereerde getallen tussen 70 en 90: " + reeks[2]);
+		System.out.println("Aantal reeksen met een som van gegenereerde getallen van 90 of meer: " + reeks[3]);
+
+	}
+
+	public static void vulReeksAan(int[] reeks, int som) {
+
+		if (som > 89) {
+			reeks[3]++;
+		} else if (som > 69) {
+			reeks[2]++;
+		} else if (som > 49) {
+			reeks[1]++;
+		} else {
+			reeks[0]++;
+		}
 	}
 
 	public static String zetOmNaarRomeinsCijfer(int randomGetal, String[] roman, int[] waarde) {
